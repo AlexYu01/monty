9f7228349e6cb880cd49f325135c3282354ab53e
stak21 <stakashima21@gmail.com>
2018-12-18 23:35:05 +0000

    added description for all functions

--- stak_funcs_2.c
+++ stak_funcs_2.c
@@ -1,4 +1,9 @@
 #include "monty.h"
+/**
+ * swap - swaps the top two elements
+ * @stack: a pointer to a pointer to the stack
+ * @line_number: holds the line the code is run
+ */
 
 void swap(stack_t **stack, unsigned int line_number)
 {
@@ -19,20 +24,47 @@ void swap(stack_t **stack, unsigned int line_number)
 	head->n = next->n;
 	next->n = temp;
 }
+/**
+ * add - adds the top two elements and pops the top and stores it in the new top
+ * @stack: a pointer to a pointer to the stack
+ * @line_number: holds the line the code is run
+ */
 
 void add(stack_t **stack, unsigned int line_number)
 {
 	int res;
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	res = (*stack)->n + (*stack)->next->n;
 	pop(stack, line_number);
 	(*stack)->n = res;
 }
+/**
+ * sub - subtract the top two elements and pops the top and stores it in the new top
+ * @stack: a pointer to a pointer to the stack
+ * @line_number: holds the line the code is run
+ */
 
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int res;
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	res = (*stack)->next->n - (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n = res;
+}
+/**
+ * nop - does nothing
+ * @stack: a pointer to a pointer to the stack
+ * @line_number: holds the line the code is run
+ */
 void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
