416cd37bb30c46ef1eddcc5d5171bc2a35eacb56
AlexYu01 <tengalexyu@gmail.com>
2018-12-19 00:12:06 +0000

    Implemented pchar

--- stak_funcs_3.c
+++ stak_funcs_3.c
@@ -1,10 +1,11 @@
 #include "monty.h"
+
 /**
  * mul - Takes the top two elements of the stack and multiplies them. Result is
  * stored at the top of the stack. The stack will be one shorter than before.
  * If an error occurs, print a message to stderr and exit with EXIT_FAILURE.
  *
- * @stack: Double pointer to the top of the stack,
+ * @stack: Double pointer to the top of the stack.
  * @line_number: Line number of where the mul opcode is in the file.
  *
  * Return: Void.
@@ -12,6 +13,7 @@
 void mul(stack_t **stack, unsigned int line_number)
 {
 	int res;
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
@@ -28,7 +30,7 @@ void mul(stack_t **stack, unsigned int line_number)
  * one shorter than before. If an error occurs, print a message to stderr and
  * exit with EXIT_FAILURE.
  *
- * @stack: Double pointer to the top of the stack,
+ * @stack: Double pointer to the top of the stack
  * @line_number: Line number of where the mod opcode is in the file.
  *
  * Return: Void.
@@ -36,6 +38,7 @@ void mul(stack_t **stack, unsigned int line_number)
 void mod(stack_t **stack, unsigned int line_number)
 {
 	int res;
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
@@ -50,4 +53,30 @@ void mod(stack_t **stack, unsigned int line_number)
 	res = (*stack)->next->n % (*stack->n);
 	pop(stack, line_number);
 	(*stack)->n = res;
+}
+
+/**
+ * pchar - Treats the integer value at the top of the stack as an ascii and
+ * prints the char equivalent.
+ * If an error is encountered, prints a message and exits with EXIT_FAILURE.
+ *
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of where the mod opcode is in the file.
+ *
+ * Return: Void.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
 }
