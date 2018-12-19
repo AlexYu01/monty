0a1f26eaa6a90c988dbe3ff79e26cef60fca0b17
stak21 <stakashima21@gmail.com>
2018-12-19 19:20:23 +0000

    Initial commit: Rotr: rotates the bottom to the top

--- stak_funcs_4.c
+++ stak_funcs_4.c
@@ -31,6 +31,33 @@ void rotl(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * rotr - Rotates the stack to the bottom, the last element becomes the top
+ *
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number of where the mul opcode is in the file.
+ *
+ * Return: Void.
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *last_ptr;
+	stack_t *head;
+	(void)line_number;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+	last_ptr = *stack;
+	head = *stack;
+	while (last_ptr->next != NULL)
+		last_ptr = last_ptr->next;
+	head->prev = last_ptr;
+	last_ptr->prev->next = NULL;
+	last_ptr->next = head;
+	last_ptr->prev = NULL;
+	*stack = last_ptr;
+	
+}
+/**
  * stack_set - Sets the format of the data to a stack. This is the default
  * behavior of the program. Anything pushed after the stack opcode will follow
  * the LIFO style.
