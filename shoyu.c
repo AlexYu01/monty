0be479823b4d6e4d3927d6039365eb4e97d847d9
AlexYu01 <tengalexyu@gmail.com>
2018-12-19 01:20:58 +0000

    Add sub to header and getops

--- shoyu.c
+++ shoyu.c
@@ -18,6 +18,7 @@ void (*get_op(char *tok))(stack_t **stack, unsigned int line_number)
 		{ "pop", pop },
 		{ "swap", swap },
 		{ "add", add },
+		{ "sub", sub },
 		{ "mul", mul },
 		{ "div", _div },
 		{ "mod", mod },
